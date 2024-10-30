001be3bdba41c8d702cbe85ee0161255fe2de32a
MYNOTEPC\kyuto <kankeyuto11.10@gmail.com>
2024-10-30 12:20:14 +0900

    TestButtonの作成

---
+++ CS_Form/CS_Form/TestButton.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Windows.Forms;
+
+namespace CS_Form
+{
+    class TestButton : Button
+    {
+
+        
+        public TestButton()
+        {
+            Click += OnClick;
+        }
+
+        public void OnClick(object sender, EventArgs s)
+        {
+            MessageBox.Show("あいうえお");
+        }
+
+    }
+}
